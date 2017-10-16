a3457b329da5742305bfc76398e82a59359434fd
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2017-10-16 21:17:24 +0300

    emit New Message and listen at client

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io(); 
+
+socket.on('connect', function () {
+    console.log('connected to server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from the server');
+});
+
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
-const port = process.env.PORT || 3000;
-
-
-const publicPath = path.join(__dirname + '../public');
+const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket)=> {
+    console.log('New user connected');
+    socket.on('disconnect', (socket) => {
+    console.log("User disconnected");
+    });
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+});
+
+
+server.listen(port, () => {
     console.log("Server is up and running on port " + port);
 })
