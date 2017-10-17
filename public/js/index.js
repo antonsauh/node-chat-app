7a41e0dc8f0ee1caee1f5efc2bc34c7d67d27f32
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2017-10-17 03:04:25 +0300

    Adding templating and moment to work with time stamps

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,14 @@ socket.on('connect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New message', message);
-    var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
-    $('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
 });
 
 socket.on('disconnect', function () {
@@ -16,12 +20,14 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newLocationMessage', function(message) {
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    })
+    $('#messages').append(html);
 });
 
  // Jquery
